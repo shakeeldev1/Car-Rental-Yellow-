3fd63fa22fb8440df80fb28653537307eb4f283b
Muhammad Shakeel <149057002+shakeel7521951@users.noreply.github.com>
2025-08-07 17:38:20 +0500

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -218,7 +218,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 60 * 60 * 1000,
     });
 
